dd10ae316da83584c2fbdaf990a84dc135fcc11e
mihiro <f.mihiroxx@gmail.com>
2023-03-07 11:03:25 +0900

    Create song model

---
+++ lib/models/song.dart
@@ -0,0 +1,13 @@
+class Song {
+  Song(
+    this.image,
+    this.title,
+    this.artist,
+    this.playtime,
+  );
+
+  final String image;
+  final String title;
+  final String artist;
+  final Duration playtime;
+}
